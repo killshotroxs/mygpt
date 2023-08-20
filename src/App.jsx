f19199b89f0a4bd6b744b8f23665cd2dd245ee86
killshotroxs <krishnal@hotmail.com>
2023-08-20 17:09:42 +0530

    typing indicator changed

--- src/App.jsx
+++ src/App.jsx
@@ -102,7 +102,7 @@ function App() {
               scrollBehavior="smooth"
               typingIndicator={
                 isTyping ? (
-                  <TypingIndicator content="ChatGPT is typing" />
+                  <TypingIndicator content="KilluGPT is typing" />
                 ) : null
               }
             >
